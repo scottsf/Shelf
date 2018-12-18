12094c7011f707f1ef3604ef819ae8cc1f7b3336
Munkhtegsh <munkhtegsh.m@gmail.com>
2018-12-17 17:14:26 -0800

    Created inputs in Form and hook them up with state

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,10 +1,35 @@
 import React, {Component} from 'react';
 
 class Form extends Component {
+  constructor() {
+    super();
+    this.state = {
+      url: '',
+      productName: '',
+      price: ''
+    }
+  }
+
+  handleInput = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+
+  }
   render() {
     return (
       <div>
-        Form
+        <img/>
+        <p>Image URL:</p>
+        <input onChange={this.handleInput} name='url' placeholder='URL'/>
+        <p>Product Name:</p>
+        <input onChange={this.handleInput} name='price' placeholder='Product'/>
+        <p>Price</p>
+        <input onChange={this.handleInput} name='productName' placeholder='Price' />
+        <div>
+          <button>Cancel</button>
+          <button>Add Inventory</button>
+        </div>
       </div>
     )
   }
