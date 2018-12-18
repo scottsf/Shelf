43583a7d16cc719aa741986184d5ae9a8bad80be
Munkhtegsh <munkhtegsh.m@gmail.com>
2018-12-17 16:48:47 -0800

    Implemented a data mapper for Node and Postgres

--- server/server.js
+++ server/server.js
@@ -4,5 +4,10 @@ let bodyParser = require('body-parser');
 let massive = require('massive');
 let app = express();
 
+app.use(bodyParser.json());
+massive(process.env.DB_URI).then(instance => {
+  app.set('db', instance);
+});
+
 let PORT = 3001;
 app.listen(PORT, () => console.log(`App is running on ${PORT}`));
