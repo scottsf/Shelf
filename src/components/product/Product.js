e506fba6f009564494beea51e07bf3a797e8e976
Munkhtegsh <munkhtegsh@outlook.com>
2018-12-20 23:07:23 -0800

    Styled Product component

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './product.scss';
 
 let Product = ({item}) => (
-  <div>
-    <img alt='' src={item.img} />
-    <p> {item.price} </p>
-    <p> {item.name} </p>
+  <div className="product">
+    <img className="product__image" alt='' src={item.img} />
+    <div className="product__p">
+      <p> {item.price} </p>
+      <p> {item.name} </p>
+    </div>
   </div>
 )
 
