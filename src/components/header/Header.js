f445b4ebc9b5dfc1c0222f74514e621ec70b6f39
Munkhtegsh <munkhtegsh@outlook.com>
2018-12-25 23:13:01 -0800

    Added navigation buttons in the Navbar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import './header.scss';
+import {Link} from 'react-router-dom';
 
 let Header = () => (
   <div className="header">
     <h4 className="header__h4"> Shelf </h4>
+    <Link to="/">
+      <button className="header__dash btn"> Dashboard </button>
+    </Link>
+    <Link to="/add">
+      <button className="header__add btn"> Add Inventory </button>
+    </Link>
   </div>
 )
 
